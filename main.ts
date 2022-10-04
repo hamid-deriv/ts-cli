87ea8cfb2e920e48ac144731a166f21440f2f1bf
Hamid Yaftian <hamid@re-work.dev>
2022-10-04 18:19:21 +0330

    Clean up

--- main.ts
+++ main.ts
@@ -1,20 +1,10 @@
 import commander from 'commander';
-import branchInit from './commands/branch';
+import commandName from './commands/command-name';
 import { name, version } from './package.json';
 
 const program = new commander.Command();
 
-branchInit(program);
-// program
-//     .command('branch')
-//     .description('test command')
-//     .option('-i, --install', 'Install node to the Project')
-//     .option('-c, --create <name>', 'Create node from stubs')
-//     .option('-l, --local', 'Install node to the Local Project')
-//     .option('-up, --update-project', 'Update Project before process command')
-//     .action(async (options) => {
-//         console.log('sss', options);
-//     });
+commandName(program);
 
 program.name(name);
 program.version(version);
